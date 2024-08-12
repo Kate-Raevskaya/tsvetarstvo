e9db8fc8940ba136cbaa89fb9f10cac54acad3ff
Ekaterina <raevskayakatya1707@gmail.com>
2024-08-12 15:51:07 +0300

    add navbar links

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,31 +2,32 @@ import { AllCategory } from "../../components/AllCategory/AllCategory"
 import { Contacts } from "../../components/Contacts/Contacts"
 import { Decor } from "../../components/Decor/Decor"
 import { Delivery } from "../../components/Delivery/Delivery"
+import "./Home.scss"
 
 export const Home = () => {
   return (
-    <>
+    <div className="home-page">
       <h1>Цветарство</h1>
 
-      <section className="category-section">
-        <h2>Категории</h2>
+      <section id="catalog-section">
+        <h2>Каталог</h2>
         <AllCategory />
       </section>
 
-      <section className="decor-section">
+      <section id="decor-section">
         <h2>Оформление</h2>
         <Decor />
       </section>
 
-      <section className="delivery-payment-section">
+      <section id="delivery-section">
         <h2>Доставка и оплата</h2>
         <Delivery />
       </section>
 
-      <section className="contacts-section">
+      <section id="contacts-section">
         <h2>Контакты</h2>
         <Contacts />
       </section>
-    </>
+    </div>
   )
 }
